914c16b06165d6732a9693082fc480ec9b448927
iturres <emanueliturres.co@gmail.com>
2023-04-20 21:06:43 -0300

    Add videos-data files

---
+++ src/js/videos-data.js
@@ -0,0 +1,287 @@
+export const videosData = {
+  items: [
+    {
+      kind: "youtube#searchResult",
+      etag: "ezzTw2UrlXQHJ7C-Z9ANzAI7oHo",
+      id: {
+        kind: "youtube#video",
+        videoId: "5TC05SqSVdc",
+      },
+      snippet: {
+        publishedAt: "2020-09-16T15:00:02Z",
+        channelId: "UCNTYu8viw2f0Qf2I27LrSVA",
+        title:
+          "#42 HPT MARRUECOS Parte 2 - CHEFCHAOUEN, El Viaje Secreto de Rulo",
+        description:
+          "Hitch Packing Thumb esta de vuelta!!! Queridos amigos suscriptores, por motivo de la pandemia que nos acontece desde ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/5TC05SqSVdc/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/5TC05SqSVdc/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/5TC05SqSVdc/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "HITCH PACKING THUMB",
+        liveBroadcastContent: "none",
+        publishTime: "2020-09-16T15:00:02Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "ovvyrV28Z4ZzEIwqLL4Oo_SpG6M",
+      id: {
+        kind: "youtube#video",
+        videoId: "FXgmZZtMOxk",
+      },
+      snippet: {
+        publishedAt: "2020-03-09T14:00:02Z",
+        channelId: "UCNTYu8viw2f0Qf2I27LrSVA",
+        title:
+          "#41 HPT ENTREVISTA EN VIVO DESDE MEKNESS MARRUECOS  &quot;El Viaje Secreto de Rulo&quot;",
+        description:
+          "QUIERES SUMARTE AL PROYECTO HPT? PARA SEGUIR EL VIAJE EN TIEMPO REAL, INGRESA A: CONTACTO: ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/FXgmZZtMOxk/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/FXgmZZtMOxk/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/FXgmZZtMOxk/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "HITCH PACKING THUMB",
+        liveBroadcastContent: "none",
+        publishTime: "2020-03-09T14:00:02Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "AZy69fmlXE7kymoBrt3LkiPwQvo",
+      id: {
+        kind: "youtube#video",
+        videoId: "gMrdxr1YB0o",
+      },
+      snippet: {
+        publishedAt: "2020-02-16T15:56:58Z",
+        channelId: "UCNTYu8viw2f0Qf2I27LrSVA",
+        title: "#40 HPT MARRUECOS Parte 1 - TANGER, El Viaje Secreto de Rulo",
+        description:
+          "QUIERES SUMARTE AL PROYECTO HPT? PARA SEGUIR EL VIAJE EN TIEMPO REAL, INGRESA A: CONTACTO: ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/gMrdxr1YB0o/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/gMrdxr1YB0o/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/gMrdxr1YB0o/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "HITCH PACKING THUMB",
+        liveBroadcastContent: "none",
+        publishTime: "2020-02-16T15:56:58Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "TdAbPEzKxJ1Ff1wV6MB7kKDYEMw",
+      id: {
+        kind: "youtube#video",
+        videoId: "bkPoynqrQgY",
+      },
+      snippet: {
+        publishedAt: "2020-01-26T18:19:24Z",
+        channelId: "UCNTYu8viw2f0Qf2I27LrSVA",
+        title: "#39 HPT Alemania Parte 3 - FRANKFURT, El Viaje Secreto de Rulo",
+        description:
+          "IMPORTANTE!!. Queridos amigos televidentes, la frecuencia con la que subo los videos todas las semanas, se verá afectada por ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/bkPoynqrQgY/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/bkPoynqrQgY/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/bkPoynqrQgY/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "HITCH PACKING THUMB",
+        liveBroadcastContent: "none",
+        publishTime: "2020-01-26T18:19:24Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "7RKgIv9IXJrr_w5W1R_zr3nDv6s",
+      id: {
+        kind: "youtube#video",
+        videoId: "6m_u9MlwElk",
+      },
+      snippet: {
+        publishedAt: "2020-01-20T15:32:21Z",
+        channelId: "UCNTYu8viw2f0Qf2I27LrSVA",
+        title:
+          "#38 HPT Alemania Parte 2 - AUTOSTOP 10.000 Km, El Viaje Secreto de Rulo",
+        description:
+          "El Autostop como herramienta de Viaje... Pero también forma parte del viajar! Ya que esta alternativa nomada y poco utilizada ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/6m_u9MlwElk/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/6m_u9MlwElk/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/6m_u9MlwElk/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "HITCH PACKING THUMB",
+        liveBroadcastContent: "none",
+        publishTime: "2020-01-20T15:32:21Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "CwVHYtYx0IK-yu6jzrZ4mSBMNwo",
+      id: {
+        kind: "youtube#video",
+        videoId: "5cPlVRnfIwY",
+      },
+      snippet: {
+        publishedAt: "2020-01-14T14:53:27Z",
+        channelId: "UCNTYu8viw2f0Qf2I27LrSVA",
+        title: "#37 HPT Alemania Parte 1 - MARBURG, El Viaje Secreto de Rulo",
+        description:
+          "PARA SEGUIR EL VIAJE EN TIEMPO REAL, INGRESA A: INSTAGRAM: Emanuel Hitch Packing Thumb / Emanuel Iturres AHI ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/5cPlVRnfIwY/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/5cPlVRnfIwY/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/5cPlVRnfIwY/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "HITCH PACKING THUMB",
+        liveBroadcastContent: "none",
+        publishTime: "2020-01-14T14:53:27Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "A0Go__cfEIcb5bRGe-cjW7cGR6s",
+      id: {
+        kind: "youtube#video",
+        videoId: "-7mQ97om3f0",
+      },
+      snippet: {
+        publishedAt: "2020-01-06T15:13:35Z",
+        channelId: "UCNTYu8viw2f0Qf2I27LrSVA",
+        title: "#36 HPT Holanda Parte 2 - ROTTERDAM, El Viaje Secreto de Rulo",
+        description:
+          "Rotterdam, una mezcla de emociones!!,una ciudad con mucha energía y la cual no estaba en mis planes de viaje, pero como el ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/-7mQ97om3f0/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/-7mQ97om3f0/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/-7mQ97om3f0/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "HITCH PACKING THUMB",
+        liveBroadcastContent: "none",
+        publishTime: "2020-01-06T15:13:35Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "Z01mtXWuqTJ89lDrW74vnHXyd8s",
+      id: {
+        kind: "youtube#video",
+        videoId: "3o8Phkr_JeM",
+      },
+      snippet: {
+        publishedAt: "2019-12-23T19:22:29Z",
+        channelId: "UCNTYu8viw2f0Qf2I27LrSVA",
+        title: "#35 HPT Holanda Parte 1 - AMSTERDAM, El Viaje Secreto de Rulo",
+        description:
+          "PARA SEGUIR EL VIAJE EN TIEMPO REAL, INGRESA A: INSTAGRAM: Emanuel Hitch Packing Thumb / Emanuel Iturres AHI ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/3o8Phkr_JeM/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/3o8Phkr_JeM/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/3o8Phkr_JeM/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "HITCH PACKING THUMB",
+        liveBroadcastContent: "none",
+        publishTime: "2019-12-23T19:22:29Z",
+      },
+    },
+  ],
+};
---
+++ src/js/youtube-data-fetch.js
@@ -0,0 +1,10 @@
+// const videosData = async () => {
+//   const response = await fetch(
+//     // "https://youtube.googleapis.com/youtube/v3/activities?part=snippet&channelId=UCNTYu8viw2f0Qf2I27LrSVA&maxResults=8&key=AIzaSyDHneAQZFXTksdB-T-jH0wjIkAoL8HtPqU"
+//   );
+//   return await response.json();
+// };
+
+// export const adminServices = {
+//   videosData,
+// };
